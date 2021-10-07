419057273e4614e6f86fef57cea8dfe5561e80fb
James Keicher <jamesdkeicher@gmail.com>
2021-10-06 18:55:43 -0700

    Completed farm animals lab.

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // import functions
 
-// reference needed DOM elements
+const paige = document.getElementById('paige')
+const monkey = document.getElementById('monkey')
+const lion = document.getElementById('lion')
+const bark = document.getElementById('dog-bark')
+const hoot = document.getElementById('monkey-sound')
+const roar = document.getElementById('lions-roar')
 
 
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+paige.addEventListener('click', () => {
+    bark.play()
+});
+monkey.addEventListener('click', () => {
+    hoot.play()
+});
+lion.addEventListener('click', () => {
+    roar.play()
+});
+
